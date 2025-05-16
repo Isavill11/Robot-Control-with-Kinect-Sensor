5f188a24328dc2e3e0add528a53f52436d992b99
Isavill11 <isav3@icloud.com>
2025-05-16 12:49:02 -0500

    moved some folders

--- control_scorbot.py
+++ control_scorbot.py
@@ -1,14 +1,28 @@
 import matlab.engine as mlab
 import time
 
-def execute_matlab_commands():
+#palm up gesture
+
+eng = mlab.start_matlab()
+
+functions = {
+    'palm_up': 'eng.Handover_block(nargout = 0)',
+    'Okay': eng.Align_three_blocks(nargout= 0),
+    'peace_sign': eng.Stack3blocks(nargout= 0),
+    'B-sign': eng.closegripper(nargout= 0),
+    'A-sign': eng.opengripper(nargout = 0),
+    'thumbs_down': eng.shutdown(nargout= 0),
+    'thumbs_up': eng.initialize(nargout= 0),
+    'C-sign': eng.Home(nargout= 0)
+}
+def execute_scorbot_command(sign):
+    sign = str(sign)
     eng = mlab.start_matlab()
-    eng.Handover_block(nargout=0)
-    time.sleep(5)
-    eng.GoHome(nargout=0)
+    functions[sign]
     eng.quit()
 
 
+execute_scorbot_command('palm_up')
 
 
 
